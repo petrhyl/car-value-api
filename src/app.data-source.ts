0b280cc225769611b28fb7b10cce2212c004f92d
Petr Hyl <kykil@seznam.cz>
2025-08-03 15:25:28 +0200

    change configuration to connect to heroku database

--- src/app.data-source.ts
+++ src/app.data-source.ts
@@ -24,12 +24,6 @@ if (dbType === "sqlite") {
     options = {
         type: dbType,
         url: process.env.DATABASE_URL,
-        host: process.env.DB_HOST || "localhost",
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-        username: process.env.DB_USER || "user",
-        password: process.env.DB_PASSWORD || "password",
-        database: process.env.DB_NAME || "database",
-        synchronize: process.env.DB_SCHEMA_SYNC === "true",
         migrationsRun: true,
         ssl: {
             rejectUnauthorized: false
